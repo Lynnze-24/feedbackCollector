0c3e0e643f324d3914ca3f4744f6386b9bb9e22d
Lynnze <aunghtetlinn.yit@gmail.com>
2023-07-10 12:18:54 +0630

    responsive restyle

--- src/app/screens/main-page/main-page.component.ts
+++ src/app/screens/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild,HostListener } from '@angular/core';
 import { AgGridAngular } from 'ag-grid-angular';
 import { ColDef } from 'ag-grid-community';
 import { Subject, Subscription } from 'rxjs';
@@ -6,6 +6,7 @@ import { feedback } from 'src/app/components/feedback-form/feedback-form.compone
 import { GridBtnComponent } from 'src/app/components/grid-btn/grid-btn.component';
 import { AuthServiceComponent } from 'src/app/services/authService/auth-service.service';
 import { fUser, UserService } from 'src/app/services/userService/user.service';
+
 
 
 
@@ -22,6 +23,7 @@ export class MainPageComponent implements OnInit,OnDestroy {
   displayFbModal:boolean = false;
   displayUserModal:boolean = false;
   hasImageError:boolean = false;
+  windowWidth:number = innerWidth;
 
   alertMessage:string = '';
 
@@ -39,19 +41,19 @@ export class MainPageComponent implements OnInit,OnDestroy {
   filterText:string = '';
 
   columnDefs: ColDef[] = [
-    { field: 'name', width: 180 },
-    { field: 'email',flex:1,
+    { field: 'name', width: this.windowWidth> 650? 120:100, },
+    { field: 'email',flex:1,minWidth:this.windowWidth> 650?180:120,
       getQuickFilterText: () => {
         return '';
       } // convert to empty string because don't wanna filter this column
     },
-    { field: 'phone', width: 150,
+    { field: 'phone',width:this.windowWidth> 650?140:110,
       getQuickFilterText: () => {
         return '';
       }// convert to empty string because don't wanna filter this column
     },
     {
-      field: 'id',headerName:'Detail', width: 150, 
+      field: 'id',headerName:'Detail', width: 130, 
       cellRenderer: GridBtnComponent, 
       sortable: false,
       getQuickFilterText: () => {
@@ -61,6 +63,10 @@ export class MainPageComponent implements OnInit,OnDestroy {
   
   ];
 
+  bigColSize: number[] = [120,180,140,130]
+  mediumColSize: number[] = [100,120,110,130]
+  smallColSize: number[] = [90,110,100,80]
+
   defaultColDef: ColDef= {
     sortable: true,
     filter: false,
@@ -68,8 +74,47 @@ export class MainPageComponent implements OnInit,OnDestroy {
     cellClass: 'no-border'  // custom css class to hide borders when click
   };
 
+
+  @HostListener('window:resize')
+  onResize() {
+    // Code to execute when window is resized
+    console.log(this.columnDefs,'resize')
+    if(innerWidth > 650){
+        this.columnDefs = this.columnDefs.map((x,i )=> {
+          if(i!==1){
+            x.width = this.bigColSize[i]
+          }else{
+            x.minWidth = this.bigColSize[i]
+          }
+          
+          return x;
+        })
+    }else if(innerWidth > 500){
+        this.columnDefs = this.columnDefs.map((x,i )=> {
+          if(i!==1){
+            x.width = this.mediumColSize[i]
+          }else{
+            x.minWidth = this.mediumColSize[i]
+          }
+          return x;
+        })
+    }else{
+        this.columnDefs = this.columnDefs.map((x,i )=> {
+          if(i!==1){
+            x.width = this.smallColSize[i]
+          }else{
+            x.minWidth = this.smallColSize[i]
+          }
+          return x;
+        })
+    }
+    
+  } 
+
   constructor(private authService:AuthServiceComponent,
               private userService:UserService) { }
+
+   
 
   ngOnInit(): void {
     this.detailSub = this.userService.onDetailClicked.subscribe(
@@ -94,6 +139,8 @@ export class MainPageComponent implements OnInit,OnDestroy {
         this.alertMessage = str;   
       }
     )
+  
+  
     
   }
 
@@ -141,6 +188,8 @@ export class MainPageComponent implements OnInit,OnDestroy {
   ngOnDestroy(){
     this.detailSub.unsubscribe()
     this.newUserSub.unsubscribe()
+
+    
   }
 
 
